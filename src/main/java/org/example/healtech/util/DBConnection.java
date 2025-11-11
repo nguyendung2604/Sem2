18cee69d3e7f18fc94146ad4f3bf1b80684930b7
nguyendung2604 <nguyendungx26@gmail.com>
2025-11-11 21:23:00 +0700

    database ddng sử dụng xam- nếu dùng docker thì thay đổi mật khẩu

--- src/main/java/org/example/healtech/util/DBConnection.java
+++ src/main/java/org/example/healtech/util/DBConnection.java
@@ -8,6 +8,7 @@ public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/QL_PhongKham";
     private static final String USER = "root";
     private static final String PASSWORD = "";
+    // nếu dùng docker thì thêm "root" vào mật khẩu
 
     public static Connection getConnection() {
         Connection connection = null;
